3a4b7a85fa03a6110a28cf8a37a6a4792ad16d9f
Kay-ddggxh <kathrin.welfare@gmail.com>
2022-09-09 15:17:27 +0000

    Create programmer class

--- sql-crud.py
+++ sql-crud.py
@@ -1,5 +1,5 @@
 from sqlalchemy import (
-    create_engine, Column, Float, ForeignKey, Integer, String
+    create_engine, Column, Integer, String
 )
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -8,6 +8,18 @@ from sqlalchemy.orm import sessionmaker
 # executing the instructions from the "chinook" database
 db = create_engine("postgresql:///chinook")
 base = declarative_base()
+
+
+# create a class-based model for the "Programmer" table
+class Programmer(base):
+    __tablename__ = "Programmer"
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    gender = Column(String)
+    nationality = Column(String)
+    famous_for = Column(String)
+
 
 # instead of connecting to the database directly, we will ask for a session
 # create a new instance of sessionmaker, then point to our engine (the db)
