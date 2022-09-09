9982e7d2cbf96892cc3e630b7bd45402fd73dec5
Kay-ddggxh <kathrin.welfare@gmail.com>
2022-09-09 12:10:50 +0000

    Create table variables for Artist, Album, Track

---
+++ sql-expression.py
@@ -0,0 +1,44 @@
+from sqlalchemy import (
+    create_engine, Table, Column, Float, ForeignKey, Integer, String, MetaData
+)
+
+# executing the instructions fro our localhost "chinook" db
+# links python file to chinook db via create_engine component
+db = create_engine("postgresql:///chinook")
+
+# MetaData class will contain a collection of our table objects
+# and associated data within those
+meta = MetaData(db)
+
+# create variable for "Artist" table
+artist_table = Table(
+    "Artist", meta,
+    Column("ArtistId", Integer, primary_key=True),
+    Column("Name", String)
+)
+
+# create variable for "Album" table
+album_table = Table(
+    "Album", meta,
+    Column("AlbumId", Integer, primary_key=True),
+    Column("Title", String),
+    Column("ArtistId", Integer, ForeignKey("artist_table.ArtistId"))
+)
+
+# create variable for "Track" table
+track_table = Table(
+    "Track", meta,
+    Column("TrackId", Integer, primary_key=True),
+    Column("Name", String),
+    Column("AlbumId", Integer, ForeignKey("album_table.AlbumId")),
+    Column("MediaTypeId", Integer, primary_key=False),
+    Column("GenreId", Integer, primary_key=False),
+    Column("Composer", String),
+    Column("Milliseconds", Integer),
+    Column("Bytes", Integer),
+    Column("UnitPrice", Float)
+)
+
+# making the connection:
+# saves our connection to the database into a variable called connection
+with db.connect() as connection:
