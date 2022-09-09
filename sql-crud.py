7000e92d76721fb67632a59f2450828b3b59bace
Kay-ddggxh <kathrin.welfare@gmail.com>
2022-09-09 15:53:40 +0000

    Add new programmers to db

--- sql-crud.py
+++ sql-crud.py
@@ -81,7 +81,12 @@ tim_berners_lee = Programmer(
 )
 
 # add each instance of our programmers to our session
-session.add(ada_lovelace)
+# session.add(ada_lovelace)
+# session.add(alan_turing)
+# session.add(grace_hopper)
+# session.add(margaret_hamilton)
+# session.add(bill_gates)
+# session.add(tim_berners_lee)
 
 # commit our session to db
 session.commit()
